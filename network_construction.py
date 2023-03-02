e7be44dd180a43c11e49c98f4cd29ec0d757d039
Onerva Korhonen <onerva.korhonen@gmail.com>
2023-03-02 14:34:44 +0200

    added reading of action-action links

--- network_construction.py
+++ network_construction.py
@@ -6,13 +6,15 @@ from functools import reduce
 from unidecode import unidecode
 import networkx as nx
 
-def read_municipal_data(path, municipality_name_key=['organization','name'], action_key='actions', action_attributes=[], indicator_level_key='indicatorLevels', indicator_type_key='level',indicator_key='indicator',indicator_attributes=[],action_to_indicator_link_key='relatedActions',action_neighbour_key='action',indicator_to_indicator_link_key='relatedCauses',indicator_neighbour_key='causalIndicator'):
+def read_municipality_data(path, municipality_name_key=['organization','name'], action_key='actions', action_attributes=[], indicator_level_key='indicatorLevels', indicator_type_key='level',indicator_key='indicator',indicator_attributes=[],action_to_action_link_key='relatedActions',action_to_indicator_link_key='relatedActions',action_neighbour_key='action',indicator_to_indicator_link_key='relatedCauses',indicator_neighbour_key='causalIndicator'):
     """
     Reads the climate actions and indicators of a municipality
-    from .json file. Note that links are read downwards: actions and
+    from .json file. Links of indicators are read downwards: actions and
     other indicators contributing to each indicator are listed as neighbours
     and link direction follows the order of hierarcy (from actions to lower-level
-    indicators to higher-level indicators).
+    indicators to higher-level indicators). Links between actions are read for 
+    each action and duplicates are removed (i.e. only link a-b is listed 
+    and b-a removed).
 
     Parameters:
     -----------
@@ -32,6 +34,7 @@ def read_municipal_data(path, municipality_name_key=['organization','name'], act
     indicator_attributes: list of strs, attributes of indicators to be read
                           from the .json file and to be included in the
                           network construction as node attributes
+    action_to_action_link_key: str, key under which the related actions are stored
     action_to_indicator_link_key: str, key under which the actions contributing to each indicator are
                                   stored
     action_neighbour_key: str, key under which the information about neighbouring actions is stored
@@ -54,13 +57,13 @@ def read_municipal_data(path, municipality_name_key=['organization','name'], act
     
     # the outermost keys reflect the database structure of the Kausal Watch
     data = data['data']['plan']
-
     # reading municipality name and removing spaces, diacritics, and capitals
     if municipality_name_key[0] in data.keys():
         municipality_name = reduce(operator.getitem, municipality_name_key, data)
         municipality_name = municipality_name.replace(' ', '_')
         municipality_name = unidecode(municipality_name).lower()
         print('Municipality name {}'.format(municipality_name))
+        print('Project name {}'.format(data['name']))
     else:
         municipality_name = ''
         print('No municipality name detected in the data, check municipality_name_key')
@@ -71,46 +74,65 @@ def read_municipal_data(path, municipality_name_key=['organization','name'], act
 
     if action_key in data.keys():
         actions = data[action_key]
+        a2a_links = action_to_action_link_key in actions[0].keys() # checking if there are action-action links
+        n_a2a = 0
         for action in actions:
+            aid = action['id']
             node_attributes = {action_attribute:action[action_attribute] for action_attribute in action_attributes}
             node_attributes['node_type'] = 'action'
-            nodes.append({action['id']:node_attributes})
+            nodes.append({aid:node_attributes})
+            # reading link information from actions
+            if a2a_links:
+                neighbours = action[action_to_action_link_key]
+                for neighbour in neighbours:
+                    if (aid,neighbour['id']) not in links and (neighbour['id'],aid) not in links:
+                        links.append((action['id'],neighbour['id']))
+                        n_a2a += 1
         print('{} actions found'.format(len(actions)))
+        if a2a_links:
+            print('{} links from actions to actions found'.format(n_a2a))
+        else:
+            print('No links from actions to actions found, check action to action link key!')
     else:
         print('No actions found, check the action key!')
 
     if indicator_level_key in data.keys():
         indicators = data[indicator_level_key]
-        for indicator in indicators:
-            if indicator_type_key in indicator.keys():
-                node_type = 'indicator_{}'.format(indicator[indicator_type_key])
-            else:
-                node_type = 'indicator'
-            node_attributes = {indicator_attribute:indicator[indicator_key][indicator_attribute] for indicator_attribute in indicator_attributes}
-            node_attributes['node_type'] = node_type
-            nodes.append({indicator[indicator_key]['id']:node_attributes})
         print('{} indicators found'.format(len(indicators)))
-
-        # reading link information from indicators
-        n_action2indicator = 0
-        n_indicator2indicator = 0
-        assert action_to_indicator_link_key in indicators[0][indicator_key].keys(), 'No links from actions to indicators found, check action to indicator link key!'
-        assert indicator_to_indicator_link_key in indicators[0][indicator_key].keys(), 'No links from indicators to indicators found, check indicator to indicator link key'
-        for indicator in indicators:
-            indicator = indicator[indicator_key]
-            iid = indicator['id']
-            neighbours = indicator[action_to_indicator_link_key]
-            for neighbour in neighbours:
-                links.append((neighbour[action_neighbour_key]['id'],iid))
-            n_action2indicator += len(neighbours)
-
-            neighbours = indicator[indicator_to_indicator_link_key]
-            for neighbour in neighbours:
-                links.append((neighbour[indicator_neighbour_key]['id'],iid))
-            n_indicator2indicator += len(neighbours)
-        print('{} links from actions to indicators found'.format(n_action2indicator))
-        print('{} links from indicators to indicators found'.format(n_indicator2indicator))
-                    
+        if len(indicators) > 0:
+            a2i_links = action_to_indicator_link_key in indicators[0][indicator_key].keys() # checking if there are action-to-indicator links
+            i2i_links = indicator_to_indicator_link_key in indicators[0][indicator_key].keys() # checking if there are action-to-action links
+            n_a2i = 0
+            n_i2i = 0
+            for indicator in indicators:
+                if indicator_type_key in indicator.keys():
+                    node_type = 'indicator_{}'.format(indicator[indicator_type_key])
+                else:
+                    node_type = 'indicator'
+                indicator = indicator[indicator_key]
+                iid = indicator['id']
+                node_attributes = {indicator_attribute:indicator[indicator_attribute] for indicator_attribute in indicator_attributes}
+                node_attributes['node_type'] = node_type
+                nodes.append({iid:node_attributes})
+                # reading link information from indicators
+                if a2i_links:
+                    neighbours = indicator[action_to_indicator_link_key]
+                    for neighbour in neighbours:
+                        links.append((neighbour[action_neighbour_key]['id'],iid))
+                    n_a2i += len(neighbours) 
+                if i2i_links:
+                    neighbours = indicator[indicator_to_indicator_link_key]
+                    for neighbour in neighbours:
+                        links.append((neighbour[indicator_neighbour_key]['id'],iid))
+                    n_i2i += len(neighbours)
+            if a2i_links:
+                print('{} links from actions to indicators found'.format(n_a2i))
+            else:
+                print('No links from actions to indicators found, check action to indicator link key!')
+            if i2i_links:
+                print('{} links from indicators to indicators found'.format(n_i2i))
+            else:
+                print('No links from indicators to indicators found, check indicator to indicator link key')
     else:
         print('No indicators found, check the indicator key!')
           
@@ -142,7 +164,7 @@ def construct_network(nodes, links, municipality_name, save_path_base=''):
         node_attributes = node[node_id]
         for attribute in node_attributes:
             G.nodes[node_id][attribute] = node_attributes[attribute]
-    if save_path:
+    if save_path_base:
         save_path = save_path_base + '/' + municipality_name + '.edg'
         nx.write_weighted_edgelist(G, save_path)
     return G
